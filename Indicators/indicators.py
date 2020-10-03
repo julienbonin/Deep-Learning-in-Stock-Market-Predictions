e61f96ac61aa4b3e6ee77b67ed0cef7845d60bf2
Julien Bonin <julienbonin@outlook.com>
2020-10-03 14:04:25 -0500

    Added comment to indicators.py

--- Indicators/indicators.py
+++ Indicators/indicators.py
@@ -1,3 +1,5 @@
+# indicators.py is just a helper file to return all indicators.
+# Necessary to call Indicators.indicaors[this file]
 from Indicators.BOLLINGER_BANDS import BOLLINGER_BANDS
 from Indicators.MACD import MACD
 from Indicators.MFI import MFI
