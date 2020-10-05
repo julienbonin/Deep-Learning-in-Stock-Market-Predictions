d63a8a8e274e5d40ba282075b6fad3eb61db5697
Julien Bonin <julienbonin@outlook.com>
2020-10-05 16:08:12 -0500

    Started working on RSI indicator

--- Indicators/RSI.py
+++ Indicators/RSI.py
@@ -2,4 +2,13 @@
 
 # Relative Strength Index
 def RSI():
-    pass
+    net = (data['close'][0] - data['open'][0]) / float(data['open'][0])
+
+    ave_gain = [net if net > 0 else 0.0]
+    ave_loss = [abs(net) if net < 0 else 0]
+    for i in range(1, len(data.index)):  #enumerate(data['open'][1:], start=1):
+        net = (data['close'][i] - data['open'][i]) / float(data['open'][i]) # percentage of this days gain or loss
+        #day_ave_gain = ((net if net > 0 else 0.0) + ave_gain[i-1]) / float(i+1)
+        #day_ave_loss = ((abs(net) if net < 0 else 0) + ave_loss[i-1]) / float(i+1) #float('-inf')
+        ave_gain.append(((net if net > 0 else 0.0) + ave_gain[i-1]) / float(i+1))
+        ave_loss.append(((abs(net) if net < 0 else 0) + ave_loss[i-1]) / float(i+1))
