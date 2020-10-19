3cfbadc7aeb7821afcad51d24636438c51dc40bd
Julien Bonin <julienbonin@outlook.com>
2020-10-19 00:12:26 -0500

    Made modifications to MFI indicator

--- Indicators/MFI.py
+++ Indicators/MFI.py
@@ -1,5 +1,21 @@
-#Link:  https://www.investopedia.com/terms/m/mfi.asp
+# Link:  https://www.investopedia.com/terms/m/mfi.asp
+import numpy as np
+
 
 # MFI - Money Flow Index
-def MFI():
-    pass
+def MFI(data, lookback_period):
+
+    col = np.zeros(len(data))
+
+    for i in range(lookback_period, len(data)):
+
+        raw_money_flow = np.array(lookback_period)
+        for j in range(lookback_period):
+            raw_money_flow[j] = ((data['high'][i-lookback_period+j] + data['low'][i-lookback_period+j] + data['low'][i-lookback_period+j]) / 3) * data['volume'][i-lookback_period+j]
+
+        money_flow_ratio = np.sum(raw_money_flow > 0) / np.sum(raw_money_flow < 0)
+
+        col[i] = 100 - (100 / (1 + money_flow_ratio))
+
+# Additional Resources
+# https://school.stockcharts.com/doku.php?id=technical_indicators:money_flow_index_mfi
