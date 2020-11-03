fe0eb272d0a4b2c9b3e64afed8a3bf151b1e9053
Julien Bonin <julienbonin@outlook.com>
2020-11-02 21:38:24 -0600

    MFI Indicator ready for review

--- Indicators/MFI.py
+++ Indicators/MFI.py
@@ -4,18 +4,26 @@ import numpy as np
 
 # MFI - Money Flow Index
 def MFI(data, lookback_period):
+    money_flow_ratio = np.zeros(len(data))
+    typical_price = (data['2. high'] + data['3. low'] + data['4. close']) / 3.0
+    raw_money_flow = typical_price * data['5. volume']
+    for j in range(lookback_period, len(raw_money_flow)):
+        pos = 1
+        neg = 1
+        for i in range(j-lookback_period, j):
+            if raw_money_flow[i+1] > raw_money_flow[i]:
+                pos += raw_money_flow[i+1]
+            elif raw_money_flow[i+1] < raw_money_flow[i]:
+                neg += raw_money_flow[i+1]
 
-    col = np.zeros(len(data))
+        money_flow_ratio[j] = pos / neg
 
-    for i in range(lookback_period, len(data)):
+    # the three lines below are for if we want to just add the column to the dataframe here
+    #col_name = lookback_period, ' day MFI'
+    #data[col_name] = 100 - (100 / (1 + money_flow_ratio))
+    #retur data
 
-        raw_money_flow = np.array(lookback_period)
-        for j in range(lookback_period):
-            raw_money_flow[j] = ((data['high'][i-lookback_period+j] + data['low'][i-lookback_period+j] + data['low'][i-lookback_period+j]) / 3) * data['volume'][i-lookback_period+j]
-
-        money_flow_ratio = np.sum(raw_money_flow > 0) / np.sum(raw_money_flow < 0)
-
-        col[i] = 100 - (100 / (1 + money_flow_ratio))
+    return 100 - (100 / (1 + money_flow_ratio))
 
 # Additional Resources
 # https://school.stockcharts.com/doku.php?id=technical_indicators:money_flow_index_mfi
